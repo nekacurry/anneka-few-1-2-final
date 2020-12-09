1435df2abde072905db548ea9684cc546461965b
nekacurry <anneka498@gmail.com>
2020-12-09 17:23:20 -0600

    added band count

--- src/MetalMeta.js
+++ src/MetalMeta.js
@@ -1,1 +1,14 @@
 import data from './metal.json'
+import React from 'react'
+
+function MetalMeta() {
+  const dataCount = Object.keys(data).length
+  return (
+    <div className="MetalMeta">
+      <h2>Metal Bands &#129304;</h2>
+            <div className="Meta-Subtitle">Bands: {dataCount}</div>
+    </div>
+  )
+}
+
+export default MetalMeta
