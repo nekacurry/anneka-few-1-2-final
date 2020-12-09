77f087b4e75b4d8e800250d02d1bcfec6912eb96
nekacurry <anneka498@gmail.com>
2020-12-09 17:08:49 -0600

    added heading

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <h1>FEW 1.2 Final Assessment</h1>
+            <div className="Title-Subtitle">Anneka Curry</div>
+    </div>
+  )
+}
+
+export default Heading
