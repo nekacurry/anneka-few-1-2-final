c51b4175e5c371948c2e377dce8b4451c56f0551
nekacurry <anneka498@gmail.com>
2020-12-09 19:01:32 -0600

    updated fans count

--- src/BandsList.js
+++ src/BandsList.js
@@ -6,12 +6,13 @@ import './BandsList.css'
 function BandsList() {
 
   const spaces = data.map(( { band_name, formed, origin, fans, split, style } ) => {
+    fans = fans * 1000
     return (
         <BandsSpace 
         name={band_name}
         formed={formed}
         origin={origin}
-        fans={fans}
+        fans={fans.toLocaleString('en')}
         split={split}
         style={style}
       />
